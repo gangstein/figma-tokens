ef844df2fbfc1730fb2d4f536c8f3c62c24f1491
Artem Shulyakov <gangstein@gmail.com>
2022-05-23 15:11:28 +0300

    clean config.js

--- config.js
+++ config.js
@@ -1,59 +1,13 @@
 const StyleDictionary = require('style-dictionary');
-const hexToRgba = require('hex-to-rgba');
+const { cssFormatter, scssFormatter } = require('./format');
 
-const formatColor = (value) => {
-  if (!value.includes('rgba')) {
-    return value;
-  }
-
-  const [left, right] = value.split(' ');
-
-  return hexToRgba(left.replace(/,/, '').replace(/rgba\(/, ''), right.slice(0, -2) / 100)
-}
-
-const formatValue = ({ type, value }) => {
-  if (type === 'boxShadow' && Array.isArray(value)) {
-    const [first] = value;
-    if (!first) {
-      return value
-    }
-
-    const directionShadow = first.type === 'innerShadow' ? 'inset ' : '';
-
-    return `${directionShadow}${first.x}px ${first.y}px ${first.blur}px ${first.spread}px ${formatColor(first.color)}`
-  }
-
-  if (type === 'color') {
-    return formatColor(value)
-  }
-
-  if (type === 'borderRadius') {
-    return `calc(${value.split(' * ').map((v, i) => i === 0 ? `${v}px` : ` * ${v}`).join('')})`
-  }
-
-  if (['fontSizes', 'sizing', 'spacing', 'borderWidth', 'letterSpacing'].includes(type)) {
-    return `${value}px`
-  }
-
-  if (['fontFamilies', 'fontWeights'].includes(type)) {
-    return `'${value}'`
-  }
-
-  return value;
-}
-
-StyleDictionary.registerFormat({
-  name: 'scssFormat',
-  formatter: function({ dictionary }) {
-    return dictionary.allTokens.map((item) => `$${item.name}: ${formatValue(item)};`).join('\n');
-  },
-});
-
-StyleDictionary.registerFormat({
-  name: 'cssFormat',
-  formatter: function({ dictionary }) {
-    return `:root {\n${dictionary.allTokens.map((item) => `\t--${item.name}: ${formatValue(item)};`).join('\n')}\n}`;
-  },
+StyleDictionary
+  .registerFormat({
+    name: 'scssFormat',
+    formatter: scssFormatter,
+  }).registerFormat({
+    name: 'cssFormat',
+    formatter: cssFormatter,
 });
 
 module.exports = {
